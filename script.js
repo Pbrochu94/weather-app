c26521f44c29240f545c26bc5ff98b5171029515
Patrick Brochu <patrickbrochu70@gmail.com>
2025-05-05 15:23:17 -0400

    Done with the fetch behavior and basic layout for header and search box section
    TODO:Polish search box section (add placeholder text
     and button text)

--- script.js
+++ script.js
@@ -11,9 +11,21 @@ let selectors = {
 
 let functions = {
   getCity: function () {
-    event.preventDefault();
-    userValues.citySearched = selectors.searchBar.value;
+    let name = selectors.searchBar.value;
+    userValues.citySearched = functions.isCityValid(name);
     console.log(userValues.citySearched);
+    domManipulations.weatherDisplay();
+  },
+  isCityValid: function (cityName) {
+    return cityName.toLowerCase();
+  },
+  get5Days: async function () {
+    let request = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userValues.citySearched}/2025-05-03/2025-05-07?unitGroup=metric&key=MJEGRF56UJXUSESDCMU4QTNLR`,
+    );
+    let jsonRequest = await request.json();
+    console.log(jsonRequest);
+    return jsonRequest.days;
   },
 };
 
@@ -22,3 +34,12 @@ let listeners = {
     selectors.button.addEventListener("click", functions.getCity);
   })(),
 };
+
+let domManipulations = {
+  weatherDisplay: async function () {
+    let array = await functions.get5Days();
+    console.log(array);
+  },
+};
+
+domManipulations.weatherDisplay();

737060978cccc81c0907e4064ae01860ce334672
Patrick Brochu <patrickbrochu70@gmail.com>
2025-05-05 13:05:00 -0400

    done with the basic button and search bar behavior
    TODO:Start the async fetch function unpon click

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Hello");
+
+let userValues = {
+  citySearched: "montreal",
+};
+
+let selectors = {
+  button: document.querySelector("button"),
+  searchBar: document.querySelector(".search-bar"),
+};
+
+let functions = {
+  getCity: function () {
+    event.preventDefault();
+    userValues.citySearched = selectors.searchBar.value;
+    console.log(userValues.citySearched);
+  },
+};
+
+let listeners = {
+  button: (function () {
+    selectors.button.addEventListener("click", functions.getCity);
+  })(),
+};

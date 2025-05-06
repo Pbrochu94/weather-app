ea87f4114bb534882c86d37a91855f69677cfc1f
Patrick Brochu <patrickbrochu70@gmail.com>
2025-05-06 11:39:25 -0400

    Done with strogin valuable informations related to Date in values object
    TODO:Keep working on the page polish visuals then
    proceed on updating cards info via fetched values

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 console.log("Hello");
 
-let userValues = {
+let values = {
+  daysOfWeekArr: [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "thursday",
+    "friday",
+    "Saturday",
+    "Sunday",
+  ],
+  currentDateInDigit: new Date().toLocaleDateString().replaceAll("/", "-"),
+  currentDay: new Date().toDateString().split(" ")[0],
   citySearched: "montreal",
 };
 
@@ -12,8 +23,8 @@ let selectors = {
 let functions = {
   getCity: function () {
     let name = selectors.searchBar.value;
-    userValues.citySearched = functions.isCityValid(name);
-    console.log(userValues.citySearched);
+    values.citySearched = functions.isCityValid(name);
+    console.log(values.citySearched);
     domManipulations.weatherDisplay();
   },
   isCityValid: function (cityName) {
@@ -21,7 +32,7 @@ let functions = {
   },
   get5Days: async function () {
     let request = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userValues.citySearched}/2025-05-04/2025-05-08?unitGroup=metric&key=MJEGRF56UJXUSESDCMU4QTNLR`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${values.citySearched}/2025-05-04/2025-05-08?unitGroup=metric&key=MJEGRF56UJXUSESDCMU4QTNLR`,
     );
     let jsonRequest = await request.json();
     console.log(jsonRequest);
@@ -42,5 +53,4 @@ let domManipulations = {
   },
 };
 
-let date = new Date();
-console.log(date.toDateString().split(" ")[0]);
+console.log(new Date().toLocaleDateString());

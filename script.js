de4e1959074b16d9be5b20e1204a34c04b1a9512
Patrick Brochu <patrickbrochu70@gmail.com>
2025-05-06 11:10:42 -0400

    Copmpleted basic visual layout for the page
    TODO:Implement the date behavior that auto set weather
    and day of the week in day cards.

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let functions = {
   },
   get5Days: async function () {
     let request = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userValues.citySearched}/2025-05-03/2025-05-07?unitGroup=metric&key=MJEGRF56UJXUSESDCMU4QTNLR`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userValues.citySearched}/2025-05-04/2025-05-08?unitGroup=metric&key=MJEGRF56UJXUSESDCMU4QTNLR`,
     );
     let jsonRequest = await request.json();
     console.log(jsonRequest);
@@ -42,4 +42,5 @@ let domManipulations = {
   },
 };
 
-domManipulations.weatherDisplay();
+let date = new Date();
+console.log(date.toDateString().split(" ")[0]);
